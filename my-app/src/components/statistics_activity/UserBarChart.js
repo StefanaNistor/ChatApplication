02c120e97e838667bb918e01290b1cd98e7ac1f3
StefanaNistor <nistorstefana21@stud.ase.ro>
2024-04-30 18:37:54 +0300

    Fixed some async issues

--- my-app/src/components/statistics_activity/UserBarChart.js
+++ my-app/src/components/statistics_activity/UserBarChart.js
@@ -7,12 +7,16 @@ function DepartmentBarChart() {
 
     const [users, setUsers] = useState([]);
     const [messages, setMessages] = useState([]);
+    const [getData, setData] = useState(false);
 
     useEffect(() => {
         getUsers();
         getMessages();
         console.log('ACtivity per user',users);
         console.log('ACtivity per user',messages);
+        if(users.length > 0 && messages.length > 0){
+            setData(true);
+        }
     }, []);
 
     const getUsers = () => {
@@ -29,40 +33,31 @@ function DepartmentBarChart() {
     };
 
     const getMessages = () => {
-        // private
-        const allMessages = []; 
-      try {
-        axios.get(
-          "http://localhost:7979/privateMessages",
-          {
+        const allMessages = [];
+    
+        axios.get("http://localhost:7979/privateMessages", {
             headers: {
-              "x-access-token": localStorage.getItem("token"),
+                "x-access-token": localStorage.getItem("token"),
             },
-          }
-        ).then((res) => {
-          console.log(res.data);
-          allMessages.push(...res.data);
+        })
+        .then((res) => {
+            console.log(res.data);
+            allMessages.push(...res.data);
+                return axios.get("http://localhost:7979/groupMessages", {
+                headers: {
+                    "x-access-token": localStorage.getItem("token"),
+                },
+            });
+        })
+        .then((res) => {
+            console.log(res.data);
+            allMessages.push(...res.data);
+            setMessages(allMessages);
+            setData(true); 
+        })
+        .catch((error) => {
+            console.error("Eroare mesage user barchart", error);
         });
-        } catch (error) {
-            console.error("ERROR GETTING PRIVATE MESSAGES", error);
-            }
-        //group
-        try {
-        axios.get("http://localhost:7979/groupMessages", {
-          headers: {
-            "x-access-token": localStorage.getItem("token"),
-          },
-        }).then((res) => {
-          console.log(res.data);
-          allMessages.push(...res.data);
-        });
-        } catch (error) {
-            console.error("ERROR GETTING GROUP MESSAGES", error);
-            }
-
-        setMessages(allMessages);
-
-
     };
 
     return (
@@ -74,30 +69,34 @@ function DepartmentBarChart() {
             marginTop: "10vh",
             marginBottom: "10vh",
           }}>
-            
-            <Bar
-                data={{
-                    labels: users.map((user) => user.username),
-                    datasets: [
-                        {
-                            label: "Number of messages per user",
-                            data: users.map((user) => messages.filter((message) => message.user_id === user.id).length),
-                            backgroundColor: "rgba(75, 192, 192, 0.6)",
-                            borderWidth: 1,
-                        },
-                    ],
-                }}
-                height={400}
-                width={600}
-                options={{
-                    maintainAspectRatio: false,
-                    scales: {
-                        y: {
-                            beginAtZero: true,
-                        },
-                    },
-                }}
-            />
+
+            {getData ? (
+                 <Bar
+                 data={{
+                     labels: users.map((user) => user.username),
+                     datasets: [
+                         {
+                             label: "Number of messages per user",
+                             data: users.map((user) => messages.filter((message) => message.user_id === user.id).length),
+                             backgroundColor: "rgba(75, 192, 192, 0.6)",
+                             borderWidth: 1,
+                         },
+                     ],
+                 }}
+                 height={400}
+                 width={600}
+                 options={{
+                     maintainAspectRatio: false,
+                     scales: {
+                         y: {
+                             beginAtZero: true,
+                         },
+                     },
+                 }}
+             /> 
+            ) : (
+                <p>Loading data...</p>
+            )}
         </div>
     );
 
