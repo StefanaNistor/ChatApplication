1e3dc79418ee197dcb8f8f6908ea5a01a814d493
StefanaNistor <nistorstefana21@stud.ase.ro>
2024-05-04 02:04:39 +0300

    updated ToDoRouter.js to separate update functions. TODO: Need to update the front handling as well

--- my-backend/routes/ToDoRouter.js
+++ my-backend/routes/ToDoRouter.js
@@ -36,6 +36,7 @@ toDoRouter.post("/addToDo", verifyToken, async (req, res) => {
   );
 });
 
+//ALL IN ONE UPDATE FUNCTION
 toDoRouter.put('/updateToDo', verifyToken, async (req, res) => {
     const { title, content, flag_id, user_id, id, start_date, end_date} = req.body;
     db.query('UPDATE todo_list SET title = $1, content = $2, flag_id = $3, user_id = $4, start_date = $6, end_date = $7 WHERE id = $5', [title, content, flag_id, user_id, id, start_date, end_date], (err, result) => {
@@ -46,6 +47,68 @@ toDoRouter.put('/updateToDo', verifyToken, async (req, res) => {
         }
     });
 });
+
+//SEPEARTED UPDATE FUNCTIONS
+//update flag
+toDoRouter.put('/updateFlag', verifyToken, async (req, res) => {
+    const { flag_id, id } = req.body;
+    db.query('UPDATE todo_list SET flag_id = $1 WHERE id = $2', [flag_id, id], (err, result) => {
+        if (!err) {
+            res.status(200).json({ message: "Flag updated successfully" });
+        } else {
+            res.status(500).json({ message: "ERROR BACKEND" });
+        }
+    });
+});
+
+//update title 
+toDoRouter.put('/updateTitle', verifyToken, async (req, res) => {
+    const { title, id } = req.body;
+    db.query('UPDATE todo_list SET title = $1 WHERE id = $2', [title, id], (err, result) => {
+        if (!err) {
+            res.status(200).json({ message: "Title updated successfully" });
+        } else {
+            res.status(500).json({ message: "ERROR BACKEND" });
+        }
+    });
+});
+
+//update content
+toDoRouter.put('/updateContent', verifyToken, async (req, res) => {
+    const { content, id } = req.body;
+    db.query('UPDATE todo_list SET content = $1 WHERE id = $2', [content, id], (err, result) => {
+        if (!err) {
+            res.status(200).json({ message: "Content updated successfully" });
+        } else {
+            res.status(500).json({ message: "ERROR BACKEND" });
+        }
+    });
+});
+
+//update start date
+toDoRouter.put('/updateStartDate', verifyToken, async (req, res) => {
+    const { start_date, id } = req.body;
+    db.query('UPDATE todo_list SET start_date = $1 WHERE id = $2', [start_date, id], (err, result) => {
+        if (!err) {
+            res.status(200).json({ message: "Start date updated successfully" });
+        } else {
+            res.status(500).json({ message: "ERROR BACKEND" });
+        }
+    });
+});
+
+//update end date
+toDoRouter.put('/updateEndDate', verifyToken, async (req, res) => {
+    const { end_date, id } = req.body;
+    db.query('UPDATE todo_list SET end_date = $1 WHERE id = $2', [end_date, id], (err, result) => {
+        if (!err) {
+            res.status(200).json({ message: "End date updated successfully" });
+        } else {
+            res.status(500).json({ message: "ERROR BACKEND" });
+        }
+    });
+});
+
 
 toDoRouter.delete('/deleteToDo/:id', verifyToken, async (req, res) => {
     const toDoID = req.params.id;
